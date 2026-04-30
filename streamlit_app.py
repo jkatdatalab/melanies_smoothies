ecd6f57d9eee73d45c1cd4070729dcba6c287473
jkatdatalab <jonathan.kipp@inform-datalab.com>
2026-04-30 10:42:42 +0200

    change connection paradigm from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title(f":cup_with_straw: Create Your Smoothie! :cup_with_straw:")
@@ -10,6 +9,7 @@ st.write(
   """
 )
 
+cnx = st.connection("snowflake")
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 
